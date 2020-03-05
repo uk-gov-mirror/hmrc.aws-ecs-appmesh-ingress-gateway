a6c1194af5d9492e5b2cb969c224d948309149c8
Ali Bahman <1422984+webit4me@users.noreply.github.com>
2020-03-05 13:20:50 +0000

    PBD-2142 Adjust replacement of the port

--- files/nginx_bootstrap.sh
+++ files/nginx_bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -ie "s/_APPLICATION_PORT/9901/" /etc/nginx/conf.d/app_nginx.conf
+sed -ie "s/_APP_PORT/${APPLICATION_PORT:-9901}/" /etc/nginx/conf.d/app_nginx.conf
 
 echo $SSL_CERT|base64 -d > /etc/ssl/certs/mdtp.pem
 echo $SSL_KEY|base64 -d > /etc/ssl/private/mdtp.key
